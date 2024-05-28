fedb793f1013bb7396e1e2f6fdb3e1fd3bfa1132
Your Name <you@example.com>
2024-05-28 22:02:05 +0100

    create middleware and a function to handle possible errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,14 +1,16 @@
 import User from "../models/user.model.js";
 import bcrytjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPassword = bcrytjs.hashSync(password, 10);
   const newUser = new User({ username, email, password: hashedPassword });
+
   try {
     await newUser.save();
     res.status(201).json("User created Successfully");
   } catch (error) {
-    res.status(500).json(error.message);
+    next(error);
   }
 };
