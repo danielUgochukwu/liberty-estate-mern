a4f58b64bdb2b0a86976d9a7952c9f21850300ac
Your Name <you@example.com>
2024-05-28 09:55:50 +0100

    create routes and pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,31 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import {
+  Home,
+  About,
+  SignIn,
+  Profile,
+  SignUp,
+  Property,
+  Contact,
+} from "./Pages/index";
+import Header from "./components/Header";
+
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/signIn" element={<SignIn />} />
+        <Route path="/signUp" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path=" /property" element={<Property />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
---
+++ client/src/Pages/index.js
@@ -0,0 +1,7 @@
+export { default as Home } from "./Home";
+export { default as About } from "./About";
+export { default as SignIn } from "./SignIn";
+export { default as Profile } from "./Profile";
+export { default as SignUp } from "./SignUp";
+export { default as Property } from "./Property";
+export { default as Contact } from "./Contact";
